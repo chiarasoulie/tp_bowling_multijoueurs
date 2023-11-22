73c4b61a67929605ae08fe5b9dcc84ebe7304809
csoul00 <souliechiara@gmail.com>
2023-11-22 15:28:21 +0100

    fin tp

--- src/main/java/bowling/PartieMultiJoueurs.java
+++ src/main/java/bowling/PartieMultiJoueurs.java
@@ -1,18 +1,89 @@
 package bowling;
-
-public class PartieMultiJoueurs extends PartieMonoJoueur implements IPartieMultiJoueurs {
-	private String[] nomsDesJoueurs;
+import java.util.HashMap;
 
 
-	public PartieMultiJoueurs(String[] nomsDesJoueurs){
-		this.nomsDesJoueurs=nomsDesJoueurs;;
+public class PartieMultiJoueurs implements IPartieMultiJoueurs {
+	private HashMap<String, PartieMonoJoueur> laPartie;
+	private String[] nomsJoueurs;
+	private int nombreDeJoueurs;
+	private int numeroJoueurActuel=-1;
+
+	private String phraseRetour;
+
+	public String getPhraseRetour() {
+		this.phraseRetour = "Prochain tir : joueur " + nomsJoueurs[numeroJoueurActuel] + ", tour n° " + laPartie.get(nomsJoueurs[numeroJoueurActuel]).numeroTourCourant() + ", boule n° " + laPartie.get(nomsJoueurs[numeroJoueurActuel]).numeroProchainLancer();
+		return phraseRetour;
 	}
 
-	public String demarreNouvellePartie(String[] nomsDesJoueurs){
-		for(var i= 0; i < nomsDesJoueurs.length; i++) {
-			System.out.println("Prochain tir : joueur "+nomsDesJoueurs[i]+" , tour n° " +numeroTourCourant()
-				+", boule n° "+i);
+	/**
+	 * Démarre une nouvelle partie pour un groupe de joueurs
+	 *
+	 * @param nomsDesJoueurs un tableau des noms de joueurs (il faut au moins un joueur)
+	 * @return une chaîne de caractères indiquant le prochain joueur,
+	 * de la forme "Prochain tir : joueur Bastide, tour n° 1, boule n° 1"
+	 * @throws java.lang.IllegalArgumentException si le tableau est vide ou null
+	 */
+
+	@Override
+	public String demarreNouvellePartie(String[] nomsDesJoueurs) throws IllegalArgumentException {
+		if (nomsDesJoueurs == null || nomsDesJoueurs.length == 0) {
+			throw new IllegalArgumentException("le tableau de noms est vide");
 		}
+
+		//initialisation
+		laPartie = new HashMap<>();
+		this.nomsJoueurs = nomsDesJoueurs;
+		nombreDeJoueurs = nomsDesJoueurs.length;
+		numeroJoueurActuel = 0;
+
+		for (String nom : nomsDesJoueurs) {
+			laPartie.put(nom, new PartieMonoJoueur());
+		}
+
+		return getPhraseRetour();
 	}
-	
+
+	/**
+	 * Enregistre le nombre de quilles abattues pour le joueur courant, dans le tour courant, pour la boule courante
+	 *
+	 * @param nombreDeQuillesAbattues : nombre de quilles abattue à ce lancer
+	 * @return une chaîne de caractères indiquant le prochain joueur,
+	 * de la forme "Prochain tir : joueur Bastide, tour n° 5, boule n° 2",
+	 * ou bien "Partie terminée" si la partie est terminée.
+	 * @throws java.lang.IllegalStateException si la partie n'est pas démarrée.
+	 */
+	@Override
+	public String enregistreLancer(int nombreDeQuillesAbattues) throws IllegalStateException {
+		if(numeroJoueurActuel==-1){
+			throw new IllegalStateException("La partie n'a pas commencé");
+		}
+
+		if (laPartie.get((nomsJoueurs[0])).numeroTourCourant()==0) {
+			return "Partie terminée";
+		}
+
+		PartieMonoJoueur joueurPartie = laPartie.get(nomsJoueurs[numeroJoueurActuel]);
+		joueurPartie.enregistreLancer(nombreDeQuillesAbattues);
+
+		if (joueurPartie.numeroProchainLancer() == 1 || joueurPartie.estTerminee()) {
+			numeroJoueurActuel = (numeroJoueurActuel + 1) % nombreDeJoueurs;
+		}
+		return getPhraseRetour();
+	}
+
+	/**
+	 * Donne le score pour le joueur playerName
+	 *
+	 * @param nomDuJoueur le nom du joueur recherché
+	 * @return le score pour ce joueur
+	 * @throws IllegalArgumentException si nomDuJoueur ne joue pas dans cette partie
+	 */
+	@Override
+	public int scorePour(String nomDuJoueur) throws IllegalArgumentException {
+		if (laPartie.get(nomDuJoueur) == null) {
+			throw new IllegalArgumentException(nomDuJoueur + "ne joue pas cette partie");
+		}
+
+		return laPartie.get(nomDuJoueur).score();
+	}
 }
---
+++ src/test/java/bowling/TestMultiJoueurs.java
@@ -0,0 +1,78 @@
+package bowling;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestMultiJoueurs{
+
+	private PartieMultiJoueurs partie;
+	private String[] nomsJoueurs;
+
+	@BeforeEach
+	public void setUp() {
+		nomsJoueurs = new String[]{"Pierre", "Paul"};
+		partie = new PartieMultiJoueurs();
+	}
+
+	@Test
+	public void testDemarrerUnePartie() {
+		assertEquals(partie.demarreNouvellePartie(nomsJoueurs), "Prochain tir : joueur Pierre, tour n° 1, boule n° 1", "la partie est mal initialisé");
+	}
+
+	@Test
+	public void testPasDeJoueurs() {
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			// On doit avoir une exception
+			partie.demarreNouvellePartie(new String[]{});
+		}, "Il n'y a pas de joueurs, la partie ne peut pas démarrer");
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			// On doit avoir une exception
+			partie.demarreNouvellePartie(null);
+		}, "Il n'y a pas de joueurs, la partie ne peut pas démarrer");
+
+	}
+
+	@Test
+	public void testPartieNonDemarree(){
+		assertThrows(IllegalStateException.class, () -> {
+			// On doit avoir une exception
+			partie.enregistreLancer(1);
+		}, "La partie n'a pas démarrer");
+	}
+
+	@Test
+	public void  testLanceEnregistre(){
+		partie.demarreNouvellePartie(nomsJoueurs);
+		assertEquals(partie.enregistreLancer(10),"Prochain tir : joueur Paul, tour n° 1, boule n° 1","pas le bon prochain tir pour le strike");
+		assertEquals(partie.enregistreLancer(4),"Prochain tir : joueur Paul, tour n° 1, boule n° 2","pas le bon prochain tir");
+		assertEquals(partie.enregistreLancer(0),"Prochain tir : joueur Pierre, tour n° 2, boule n° 1","pas le bon prochain tir");
+	}
+
+	@Test
+	public void testPartieTerminee(){
+		partie.demarreNouvellePartie(nomsJoueurs);
+		for(int i=0;i<41;i++){
+			partie.enregistreLancer(5);
+		}
+		assertEquals(partie.enregistreLancer(4),"Partie terminée","la partie ne se termine pas");
+	}
+	@Test
+	public void testScoreStrikeSpare(){
+		partie.demarreNouvellePartie(nomsJoueurs);
+		partie.enregistreLancer(10);
+		partie.enregistreLancer(5);
+		partie.enregistreLancer(5);
+		partie.enregistreLancer(2);
+		partie.enregistreLancer(3);
+		partie.enregistreLancer(1);
+		partie.enregistreLancer(0);
+		assertEquals(partie.scorePour("Pierre"),20,"pas le bon score lors d'un strike");
+		assertEquals(partie.scorePour("Paul"),12,"pas le bon score lors d'un spare");
+	}
+
+}
